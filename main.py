4a0afc2594409e3ac2ef3ed00d09d4a986261cbe
vvvadim <114286694+cestxvcdim@users.noreply.github.com>
2023-02-12 19:19:14 +0300

    fy 4 kata

--- main.py
+++ main.py
@@ -1,12 +1,66 @@
+# Too slow
+# def pi(k):
+#     n = 10 ** 10_010
+#     m = p = 2 * n
+#     i = 1
+#     while m:
+#         m = m * i // (2 * i + 1)
+#         p += m
+#         i += 1
+#     return int(str(p)[k])
+
+
+# print(pi(10_000))
+
+
+import math
+# Adaptation from craig-wood.com/nick/articles/pi-chudnovsky/
 def pi(k):
-    n = 10 ** 10_010
-    m = p = 2 * n
-    i = 1
-    while m:
-        m = m * i // (2 * i + 1)
-        p += m
-        i += 1
-    return int(str(p)[k])
+    k += 100
+    C = 640320
+    C3_OVER_24 = C**3 // 24
+    def bs(a, b):
+        if b - a == 1:
+            # Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
+            if a == 0:
+                Pab = Qab = 1
+            else:
+                Pab = (6*a-5)*(2*a-1)*(6*a-1)
+                Qab = a*a*a*C3_OVER_24
+            Tab = Pab * (13591409 + 545140134*a) # a(a) * p(a)
+            if a & 1:
+                Tab = -Tab
+        else:
+            # Recursively compute P(a,b), Q(a,b) and T(a,b)
+            # m is the midpoint of a and b
+            m = (a + b) // 2
+            # Recursively calculate P(a,m), Q(a,m) and T(a,m)
+            Pam, Qam, Tam = bs(a, m)
+            # Recursively calculate P(m,b), Q(m,b) and T(m,b)
+            Pmb, Qmb, Tmb = bs(m, b)
+            # Now combine
+            Pab = Pam * Pmb
+            Qab = Qam * Qmb
+            Tab = Qmb * Tam + Pam * Tmb
+        return Pab, Qab, Tab
+    # how many terms to compute
+    DIGITS_PER_TERM = math.log10(C3_OVER_24/6/2/6)
+    N = int(k/DIGITS_PER_TERM + 1)
+    # Calclate P(0,N) and Q(0,N)
+    P, Q, T = bs(0, N)
+    one = 10**k
+    sqrtC = sqrt(10005*one, one)
+    k -= 100
+    return int(str((Q*426880*sqrtC) // T)[k])
 
-
-print(pi(10_000))
+def sqrt(n, one):
+    floating_point_precision = 10**16
+    n_float = float((n * floating_point_precision) // one) / floating_point_precision
+    x = (int(floating_point_precision * math.sqrt(n_float)) * one) // floating_point_precision
+    n_one = n * one
+    while 1:
+        x_old = x
+        x = (x + n_one // x) // 2
+        if x == x_old:
+            break
+    return x
